3bf3bef63a426a49c388e4f2cd22a7d2ceccead8
Jsquad <lakis006@umn.edu>
2020-08-25 02:24:12 -0500

    added the bootstrap styling to hero/jumbotron

--- portfolio-app/src/components/Hero.js
+++ portfolio-app/src/components/Hero.js
@@ -7,13 +7,13 @@ import Row from 'react-bootstrap/Row';
 
 function Hero(props) {
     return (
-        <Jumbotron>
-            <Container>
-                <Row>
-                    <Col>
-                        {props.title && <h1>{props.title}</h1>}
-                        {props.subTitle && <h3>{props.subTitle}</h3>}
-                        {props.text && <h3>{props.text}</h3>}
+        <Jumbotron className="bg-transparent jumbotron-fluid p-0">
+            <Container fluid={true}>
+                <Row className="justify-content-center py-5">
+                    <Col md={8} sm={6}>
+                        {props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1>}
+                        {props.subTitle && <h3 className="display-1 font-weight-light">{props.subTitle}</h3>}
+                        {props.text && <h3 className="lead font-weight-light">{props.text}</h3>}
                     </Col>
                 </Row>
             </Container>
