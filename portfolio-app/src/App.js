1119790bdcec189cb72f0b26e634ec6fc8e65f62
Jsquad <lakis006@umn.edu>
2020-08-25 01:06:49 -0500

    added the constructor to take in props for title and header links

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { // taking in the 
+      title: "Jamal Lakis",
+      headerLinks: [
+
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Contact', path: '/contact' }
+      ],
+      // adding top level info for each page in particular 
+      home: {
+        title: 'Enjoy Life',
+        subTitle: 'Projects that make users happy',
+        text: "Checkout The Projects!"
+      },
+      abour: {
+        title: 'About Me'        
+      },
+      contact: {
+        title: 'Let\'s Chat'
+      }
+    }
+  }
+  render() {
+    return (
+      <div>Hello From React</div>
+    )
+  }
 }
 
 export default App;
