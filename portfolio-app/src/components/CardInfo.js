6f61f1809398dc8f04b28e616bacb58ec213a998
Jsquad <lakis006@umn.edu>
2020-08-25 03:52:59 -0500

    finished adding the css to the cards in cardInfo.js and started testing the click method on the cards

--- portfolio-app/src/components/CardInfo.js
+++ portfolio-app/src/components/CardInfo.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useSpring, animated } from 'react-spring';
 
- function CardInfo() {
+ function CardInfo(props) {
+
+    // the const below is actually going to hold the style of react spring we're using 
+    const style = useSpring({opacity: 1, from: {opacity: 0}});
+
+
     return (
-      <p>Card info</p>
+    
+      <animated.div className="g-card-info" style={style}>
+          <p className="g-card-title">{props.title}</p>
+          <p className="g-card-sub-title">{props.subTitle}</p>
+          <a href={props.link} target="_blank" rel="noopener norefferer">View</a>
+      </animated.div>
     )
 }
 
