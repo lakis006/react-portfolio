e13441466b2179f465a0f76282f3beda81341f5a
Jsquad <lakis006@umn.edu>
2020-08-25 04:51:18 -0500

    added the constructor which will take in the form elements such as name, email, message, etc.. right now it console.logs the form and I still need to get it actually render/ send me an email

--- portfolio-app/src/pages/ContactPage.js
+++ portfolio-app/src/pages/ContactPage.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Form from 'react-bootstrap/From';
+import Content from '../components/Content';
+import Button from 'react-bootstrap/Button';
 
 class ContactPage extends React.Component {
 
@@ -13,9 +16,51 @@ class ContactPage extends React.Component {
       }
    }
 
+   handleChange = (event) => {
+      console.log(event);
+
+      const target = event.target;
+      const value = target.type === 'checkbox' ? target.checked : target.value;
+      const name = target.name;
+      
+      this.setState({
+         [name] : value
+      })
+   }
+
    render() {
       return (
-         <p>ContactPage works</p>
+         <div>
+            <Hero title={this.props.title} />
+
+            <Content>
+               <Form onSubmit={this.handleSubmit}>
+                  <Form.Group>
+                     <Form.Label htmlFor="full-name">Full Name</Form.Label>
+                     <Form.Control id="full-name" name="name" type="text" value={this.state.name} onChange={this.handleChange}></Form.Control>
+                  </Form.Group>
+
+                  <Form.Group>
+                     <Form.Label htmlFor="email">Email</Form.Label>
+                     <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange}></Form.Control>
+                  </Form.Group>
+
+                  <Form.Group>
+                     <Form.Label htmlFor="message">Email</Form.Label>
+                     <Form.Control id="message" name="message" as="textarea" rows="3" value={this.state.message} onChange={this.handleChange}></Form.Control>
+                  </Form.Group>
+
+                  <Button className="d-inline-block" variant="primary" type="submit" disabled={this.state.disabled}>
+                     Send
+                     </Button>
+
+
+                     {/* below we're adding feedback to the user to make sure email was sent successfully */}
+                     {this.state.emailSent === true && <p className="d-inline success-msg">Email Sent</p>}
+                     {this.state.emailSent === false && <p className="d-inline err-msg">Email Not Sent</p>}
+               </Form>
+            </Content>
+         </div>
       )
    }
 
